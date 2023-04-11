08d2fc095af45afa9169956e3b4b8783a2e140f5
DCoder-99 <dcoder.work@gmail.com>
2023-04-11 23:46:44 +0700

    create: template react tailwind

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className='flex items-center justify-center'>Hello, Code here <span className='italic text-lg font-semibold ml-4'> --DCoder--</span></div>
+  );
+}
+
+export default App;
